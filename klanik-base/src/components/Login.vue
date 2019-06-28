527b7255cdebe43dbd547d78efd5f2bdbfa391e7
MagBa <magos.barnabe@gmail.com>
2019-06-28 08:05:21 +0200

    Word

--- klanik-base/src/components/Login.vue
+++ klanik-base/src/components/Login.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="login-box">
-        
-
+  <div class="login-box">
         <div class="login" v-if="!loggedIn">
             <router-link :to="{name:'register'}">Register</router-link>
             <input v-model='username' type="text" placeholder="UserName">
@@ -9,99 +7,87 @@
             <button @click="login" >Login</button>
             <router-link :to="{name:'forgot'}">I forgot my password</router-link>
         </div>
-        
         <div class="logout" v-if="loggedIn">
              <button @click="logout">Log out</button>
         </div>
-
-    </div>
-
+  </div>
 </template>
 
 
 <script>
 import authenticationService from "@/services/AuthenticationService";
-import {authHeader} from '../tools/auth-header';
-
-
+import { authHeader } from "../tools/auth-header";
 
 export default {
-    name: "login",
-    components:{
-    },
-    data(){
-        return {
-            username:'',
-            password: '',
-            returnUrl :'',
-            error:'',
-        }
-    },
-    created(){
-        this.returnUrl = this.$route.query.returnUrl ||'/';
-    },
-    async mounted(){
-        console.log("Mounting login.vue");
-        var tokenBearer = authHeader(); //should be "bearer myEncryptedToken"
+  name: "login",
+  components: {},
+  data() {
+    return {
+      username: "",
+      password: "",
+      returnUrl: "",
+      error: ""
+    };
+  },
+  created() {
+    this.returnUrl = this.$route.query.returnUrl || "/";
+  },
+  async mounted() {
 
-        if(tokenBearer!= undefined){
-            var myHeader = {
-                    'Content-Type':'application/json',
-                    'Authorization':tokenBearer
-            }
+    var tokenBearer = authHeader(); //should be "bearer myEncryptedToken"
 
-            //console.log("Header : ", myHeader);
-            
-            await authenticationService.connect(myHeader)
-                .then(user => {
-                    this.$router.push(this.returnUrl);
-                });  
-        }
-    },
-    computed:{
-        loginModel(){
-            return {
-                username : this.username,
-                password : this.password
-            };
-        },
-        loggedIn(){
-            return this.$store.getters.isLogged;
-        },
-       
-    },
-    methods:{
-        async login(){
-            await authenticationService.login(this.loginModel)
-                .then(
-                    user =>{
-                        this.$router.push(this.returnUrl);
-                    } ,
-                    error => {
-                        this.error = error;
-                        this.loading = false;
-                    }
-                );
-        },
-        logout(){
-            authenticationService.logout();
-            this.$router.push('/login');
-        }
-    },
-    watch:{
+    if (tokenBearer != undefined) {
+      var myHeader = {
+        "Content-Type": "application/json",
+        Authorization: tokenBearer
+      };
 
+      //console.log("Header : ", myHeader);
+
+      await authenticationService.connect(myHeader).then(user => {
+        this.$router.push(this.returnUrl);
+      });
     }
-}
+  },
+  computed: {
+    loginModel() {
+      return {
+        username: this.username,
+        password: this.password
+      };
+    },
+    loggedIn() {
+      return this.$store.getters.isLogged;
+    }
+  },
+  methods: {
+    async login() {
+      await authenticationService.login(this.loginModel).then(
+        user => {
+          this.$router.push(this.returnUrl);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
+    },
+    logout() {
+      authenticationService.logout();
+      this.$router.push("/login");
+    }
+  },
+  watch: {}
+};
 </script>
 
 <style>
-    .box{
-        display:inline;
-    }
+.box {
+  display: inline;
+}
 
-    .login input{
-        max-width:100px;
-    }
-
+.login input {
+  max-width: 100px;
+}
 </style>
 
